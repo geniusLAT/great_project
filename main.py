f8bf2cd776cb2a52148767e09c4cf0bb671852e4
a.pshenichnikova <a.pshenichnikova@urfu.me>
2023-12-20 10:13:38 +0500

    Multiuser bot

--- main.py
+++ main.py
@@ -2,14 +2,55 @@
 from transformers import pipeline
 import telebot
 
+user_datas=[]
+
+class user_data(object):
+    def __init__(self,id) -> None:
+        self.latitude=None
+        self.longitude=None
+
+        self.request= None
+        self.user_id=id
+
+    def set_coordinate(self,lat,lon):
+        self.latitude=lat
+        self.longitude=lon
+
+    def set_request(self, request):
+        self.request=request
+
+    def check(self):
+        return (self.longitude!=None) and(self.latitude!=None) and (self.request!=None) 
+
+def get_user_data(id):
+    ud = None
+    for data in user_datas:
+        if data.user_id==id:
+            ud=data
+
+    if ud==None:
+        ud = user_data(id)
+        user_datas.append(ud)
+    return ud
+
 token= '6968662842:AAF6j6Pxy3cNbntdrhD1zlawnFD2lPJXFds'
 
 bot=telebot.TeleBot(token)
 
 
+def process(ud):
+    print("process")
+
+
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    bot.send_message(message.chat.id,str(message.chat.id))
+    ud = get_user_data(message.chat.id)
+    ud.set_request(message.text)
+    if ud.check():
+        bot.send_message(message.chat.id,str('Бот работает. Ожидайте'))
+        process(ud)
+    else:
+        bot.send_message(message.chat.id,str('Пришлите локацию через телеграм'))
 
 '''
 @bot.message_handler(commands=["geo"])
@@ -26,6 +67,13 @@ def location(message):
         print(message.location)
         print("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
         bot.send_message(message.chat.id,str("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude)))
+        ud = get_user_data(message.chat.id)
+        ud.set_coordinate(message.location.latitude,message.location.longitude)
+        if ud.check():
+            bot.send_message(message.chat.id,str('Бот работает. Ожидайте'))
+            process(ud)
+        else:
+            bot.send_message(message.chat.id,str('Напишите ваш текстовый запрос'))
 
 '''
 def AI_example():
